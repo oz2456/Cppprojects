4cbc06f20e30a7bf206a01ccd09ba59eeec85a2f
MOHD OZAIR QURESHI <63601157+oz2456@users.noreply.github.com>
2023-02-24 12:12:42 +0530

    Create Rizz.cpp
    welcome back shady

---
+++ Rizz.cpp
@@ -0,0 +1,14 @@
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main(){
+string s("");
+cin >> s ;
+if(s == "pull girls"){
+	cout <<"congrats you are now rizz " ;
+}else{
+		cout << "death by rizzler" ;
+	}
+cout << "\ngirl or no girl rizzler is permanent !!!!!!";
+return 0;
+}
